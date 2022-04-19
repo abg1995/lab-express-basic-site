1fa4ecfdd88f258b77a7025f0faa60e8adef5ae9
alejandro blanco <alejandro@alejandros-MacBook-Air-2.local>
2022-04-19 17:45:15 +0200

    initial css styles, nav bar and home.html complleted

--- app.js
+++ app.js
@@ -5,16 +5,16 @@ app.use(express.static("public"));
 
 
 app.get("/home", (req, res, next) => {
-    res.sendFile(__dirname +'/view/home.html')
-})
+    res.sendFile(__dirname +'/view/home.html');
+});
 
 app.get("/about", (req, res, next) => {
-    res.sendFile(__dirname +'/view/about.html')
-})
+    res.sendFile(__dirname +'/view/about.html');
+});
 
 app.get("/works", (req, res, next) => {
-    res.sendFile(__dirname +'/view/works.html')
-})
+    res.sendFile(__dirname +'/view/works.html');
+});
 
 app.listen(3000, () => {
     console.log("listening")
