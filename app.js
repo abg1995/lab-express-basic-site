465b04ca9a44c3bce40b3109e7d77f214c621d12
mafaldao21 <mafalda.oliveira.21@icloud.com>
2022-04-19 16:26:15 +0100

    add app js

--- app.js
+++ app.js
@@ -2,3 +2,20 @@ const express = require("express");
 const app =  express();
 
 app.use(express.static("public"));
+
+
+app.get("/home", (req, res, next) => {
+    res.sendFile(__dirname +'/view/home.html')
+})
+
+app.get("/about", (req, res, next) => {
+    res.sendFile(__dirname +'/view/about.html')
+})
+
+app.get("/works", (req, res, next) => {
+    res.sendFile(__dirname +'/view/works.html')
+})
+
+app.listen(3000, () => {
+    console.log("listening")
+})
